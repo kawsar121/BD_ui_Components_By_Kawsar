bf5c752426017b2489159e561b3d9f152fdf087a
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2026-01-20 16:02:20 +0600

    change button ui

--- src/app/(Pages)/PublicComponets/clipbord/layout.js
+++ src/app/(Pages)/PublicComponets/clipbord/layout.js
@@ -5,8 +5,8 @@ function layout({children}) {
   return (
       <div className='border-2  flex flex-col justify-center items-center p-5'>
         <div className='flex gap-5 border-2 px-52 py-2'>
-            <button className='btn bg-amber-200 text-black'><Link href="/PublicComponets/clipbord/clipprivew">Privew</Link></button>
-            <button className='btn bg-amber-200 text-black'><Link href="/PublicComponets/clipbord/clipCode">Code</Link></button>
+            <button className='btn btn-outline btn-secondary'><Link href="/PublicComponets/clipbord/clipprivew">Privew</Link></button>
+            <button className='btn btn-outline btn-secondary '><Link href="/PublicComponets/clipbord/clipCode">Code</Link></button>
         </div>
       <div>
         {children}
