7139a7d23354603c76abd05e33ccf683e8fc6016
kawsar121 <tohidulislamkawsarbhuiyan@gmail.com>
2026-01-26 17:05:41 +0600

    carousel dropdown button complete

--- src/app/(Pages)/PublicComponets/carousels/Carousel2/layout.jsx
+++ src/app/(Pages)/PublicComponets/carousels/Carousel2/layout.jsx
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
-function layout() {
+function Carousel2Layout({children}) {
   return (
-    <div>layout</div>
+    <div className='border-2  flex flex-col justify-center items-center p-5'>
+        <div className=' flex gap-5 px-52 py-2'>
+            <button className='btn btn-outline btn-secondary'><Link href="/PublicComponets/Carousel1/Carousel1Privew">Privew</Link></button>
+            <button className='btn btn-outline btn-secondary'><Link href="/PublicComponets/Carousel1/Carousel1Code">Code</Link></button>
+        </div>
+      <div>
+        {children}
+      </div>
+    </div>
   )
 }
 
-export default layout
+export default Carousel2Layout
